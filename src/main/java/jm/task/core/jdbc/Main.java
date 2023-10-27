bf5ffb37b2a278b2d9b9ddec69584d1ceea004b3
stalseq <136884591+stalseq@users.noreply.github.com>
2023-10-27 21:16:53 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,18 +1,14 @@
 package jm.task.core.jdbc;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.model.User;
+
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-import org.hibernate.Session;
 
 import java.sql.SQLException;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
 
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Dwayne", "Johnson", (byte) 51);
         userService.saveUser("Vin", "Diesel", (byte) 55);
@@ -21,6 +17,5 @@ public class Main {
         System.out.println(userService.getAllUsers());
         userService.cleanUsersTable();
         userService.dropUsersTable();
-
     }
 }
